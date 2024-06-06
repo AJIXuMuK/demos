cfda8aae1deae2e69eb3bb57cbea8f7c032d4785
Alex Terentiev <aterentiev@microsoft.com>
2024-06-06 12:52:20 -0700

    update

--- spfx-in-m365/src/webparts/ruleThemAll/RuleThemAllWebPart.ts
+++ spfx-in-m365/src/webparts/ruleThemAll/RuleThemAllWebPart.ts
@@ -52,6 +52,7 @@ export default class RuleThemAllWebPart extends BaseClientSideWebPart<IRuleThemA
               environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentOutlook : strings.AppOutlookEnvironment;
               break;
             case 'Teams': // running in Teams
+            case 'TeamsModern':
               environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentTeams : strings.AppTeamsTabEnvironment;
               break;
             default:
